10579b2cf31349b5fde47e9658d2d630074f6854
Diego Bryan <thediegobryan@gmail.com>
2024-04-22 09:45:07 -0400

    added type to form data

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -2,14 +2,14 @@
 import { Pet } from "@/lib/types";
 import prisma from "@/lib/db";
 
-export async function addPet(formData) {
+export async function addPet(formData: FormData) {
   await prisma.pet.create({
     data: {
-      name: formData.get("name"),
-      ownerName: formData.get("ownerName"),
-      imageUrl: formData.get("imageUrl"),
+      name: formData.get("name") as string,
+      ownerName: formData.get("ownerName") as string,
+      imageUrl: formData.get("imageUrl") as string,
       age: Number(formData.get("age")),
-      notes: formData.get("notes"),
+      notes: formData.get("notes") as string,
     },
   });
 }
